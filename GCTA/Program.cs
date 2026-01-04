edde856463c339ef08fb7ca11a306d4d5c2b29b6
furkanozturk <monsterfurkan33@gmail.com>
2026-01-04 15:03:25 +0300

    battle system updated, trainer system added

--- GCTA/Program.cs
+++ GCTA/Program.cs
@@ -54,6 +54,7 @@
         {
             Console.WriteLine(IsFainted() ? $"{Name} has fainted!" : $"{Name} has [{Health}/{MaxHealth}] HP and [{CurrentEnergy}] energy.");
         }
+      
         public void UseMove(int moveIndex, Pokemon targetPokemon)
         {
             var move = Moves[moveIndex];
@@ -89,6 +90,54 @@
         }
 
     }
+    class Trainer
+    {
+        public string Name { get; set; }
+        public List<Pokemon> Tpokemon = new List<Pokemon>();
+        public void AddPokemon(Pokemon pokemon)
+        {
+            Tpokemon.Add(pokemon);
+        }
+        public void ShowTeam()
+        {
+            for(int i = 0; i<Tpokemon.Count;i++)
+            {
+                Console.WriteLine($"{i}: {Tpokemon[i].Name} (HP: {Tpokemon[i].Health}/{Tpokemon[i].MaxHealth}) [{Tpokemon[i].IsFainted()}]");
+            }
+        }
+        public Pokemon GetPokemon(int index)
+        {
+            if(index < 0 || index >= Tpokemon.Count)
+            {
+                Console.WriteLine("Invalid index!");
+                return null;
+            }
+
+            if(Tpokemon[index].IsFainted())
+            {
+                Console.WriteLine($"{Tpokemon[index].Name} has fainted!");
+                return null;
+            }   
+            return Tpokemon[index];
+
+
+        }
+        public bool HasAlivePokemon()
+        {
+            foreach (var pokemon in Tpokemon)
+            {
+                if (!pokemon.IsFainted())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Trainer()
+        {
+            Name = "Ash";
+        }
+    }
     class BattleCalculator
     {
         public static float ElementMultiplier(ElementType attackerType, ElementType defenderType)
@@ -110,51 +159,77 @@
     {
         public static void Main(string[] args)
         {
-            Random rnd = new Random();
-            //Pikachu: Thundershock,Thunderbolt,Quick Attack hp70
-            //Charmander: Ember,Flamethrower,Scratch    hp80
-            //Squirtle: Water Gun,Hydro Pump,Tackle     hp70
-            Pokemon charmander = new Pokemon("Charmander", 80, ElementType.Fire,
-                new Move("Scratch", 10, ElementType.Normal, 0, 20),
-                new Move("Ember", 20, ElementType.Fire, 10, 5),
-                new Move("Flamethrower", 35, ElementType.Fire, 30, 0)
+            Trainer playerTrainer = new Trainer();
+            Trainer enemyTrainer = new Trainer();
+            Pokemon charmander = new Pokemon("Charmander", 200, ElementType.Fire,
+                new Move("Scratch", 30, ElementType.Normal, 0, 15),
+                new Move("Ember", 55, ElementType.Fire, 30, 0),
+                new Move("Flamethrower", 150, ElementType.Fire, 54, 0)
                 );
-            Pokemon squirtle = new Pokemon("Squirtle", 70, ElementType.Water,
-                new Move("Tackle", 5, ElementType.Normal, 0, 30),
-                new Move("Water Gun", 15, ElementType.Water, 10, 5),
-                new Move("Hydro Pump", 30, ElementType.Water, 35, 0)
+            Pokemon squirtle = new Pokemon("Squirtle", 200, ElementType.Water,
+                new Move("Tackle", 20, ElementType.Normal, 0, 30),
+                new Move("Water Gun", 45, ElementType.Water, 20, 5),
+                new Move("Hydro Pump", 90, ElementType.Water, 50, 0)
                 );
-            while (!charmander.IsFainted() && !squirtle.IsFainted())
+            Pokemon pikachu = new Pokemon("Pikachu", 200, ElementType.Electric,
+                new Move("Quick Attack", 25, ElementType.Normal, 0, 25),
+                new Move("Thunder Shock", 40, ElementType.Electric, 30, 5),
+                new Move("Thunderbolt", 100, ElementType.Electric, 45, 0)
+                );
+            Pokemon flareon = new Pokemon("Flareon", 210, ElementType.Fire,
+                new Move("Flaming Breath", 20, ElementType.Normal, 0, 25),
+                new Move("Flamethrower", 70, ElementType.Fire, 30, 5),
+                new Move("Scorching Column", 120, ElementType.Fire, 60, 0)
+                );
+            Pokemon leafeon = new Pokemon("Leafeon", 210, ElementType.Grass,
+                new Move("Soothing Scent", 30, ElementType.Normal, 0, 15),
+                new Move("Magical Leaf", 50, ElementType.Grass, 20, 5),
+                new Move("Slashing Strike", 140, ElementType.Grass, 60, 0)
+                );
+            Pokemon vaporeon = new Pokemon("Vaporeon", 210, ElementType.Water,
+                new Move("Spiral Drain", 30, ElementType.Water, 0, 20),
+                new Move("Aqua Sonic", 50, ElementType.Water, 25, 5),
+                new Move("Splash Jump", 90, ElementType.Water, 55, 0)
+                );
+            playerTrainer.AddPokemon(pikachu);
+            playerTrainer.AddPokemon(vaporeon);
+            playerTrainer.AddPokemon(charmander);
+            enemyTrainer.AddPokemon(squirtle);
+            enemyTrainer.AddPokemon(flareon);
+            enemyTrainer.AddPokemon(leafeon);
+            while (playerTrainer.HasAlivePokemon() && enemyTrainer.HasAlivePokemon())
             {
-                int randomIndex = rnd.Next(0, 3);
-                charmander.ShowStatus();
-                squirtle.ShowStatus();
-                Console.WriteLine($"{charmander.Name}, choose a move:");
-                for(int i = 0; i < charmander.Moves.Count; i++)
+                for (int i = 0; i < playerTrainer.Tpokemon.Count; i++)
                 {
-                    var move = charmander.Moves[i];
-                    Console.WriteLine($"{i}: {move.Name} (Damage: {move.Damage}, Type: {move.Type}, Energy Cost: {move.EnergyCost}, Energy Gain: {move.EnergyGain})");
-                }
-                Console.Write("Choice: ");
-                int choice = int.Parse(Console.ReadLine());
-                charmander.UseMove(choice, squirtle);
-                if (squirtle.IsFainted())
-                {
-                    Console.WriteLine($"{squirtle.Name} has fainted! {charmander.Name} wins!");
-                    break;
-                }
-                squirtle.UseMove(randomIndex, charmander);
-                if (charmander.IsFainted())
-                {
-                    Console.WriteLine($"{charmander.Name} has fainted! {squirtle.Name} wins!");
-                    break;
-                }
-                Console.WriteLine("Press a key to continue...");
-                Console.ReadKey();
-                Console.Clear();
+                    var attacker = playerTrainer.Tpokemon[i];
+                    if (attacker.IsFainted())
+                        continue;
+                    Console.WriteLine($"Its {attacker.Name}'s turn.");
+                    enemyTrainer.ShowTeam();
+                    Console.Write("Who are you going to attack?\n: ");
+                    int index = int.Parse(Console.ReadLine() ?? "0");
+                    var target = enemyTrainer.Tpokemon[index];
+                    for (int j = 0; j < attacker.Moves.Count; j++)
+                    {
+                        var move = attacker.Moves[j];
+                        Console.WriteLine($"{j}: {move.Name} (Damage: {move.Damage}, Type: {move.Type}, Energy Cost: {move.EnergyCost}, Energy Gain: {move.EnergyGain})");
+                    }
+                    Console.WriteLine("Which skill will you use?: ");
+                    int chosenSkill = int.Parse(Console.ReadLine());
+                    attacker.UseMove(chosenSkill, target);
 
+                }
+                for (int i = 0; i < enemyTrainer.Tpokemon.Count; i++)
+                {
+                    var attacker = enemyTrainer.Tpokemon[i];
+                    if (attacker.IsFainted())
+                        continue;
+                    var alivePokemons = playerTrainer.Tpokemon.Where(p => !p.IsFainted()).ToList();//
+                    var target = alivePokemons[new Random().Next(alivePokemons.Count)];
+                    var moveIndex = new Random().Next(attacker.Moves.Count);
+                    attacker.UseMove(moveIndex, target);
+                }
             }
-
         }
     }
 }
